ef0d3f7da75823dba51824495915cb8dc1b3c6cd
Mads Skytte Nielsen <au536099@uni.au.dk>
2019-04-06 14:20:12 +0200

    Stepped up to step 8.

---
+++ Microwave.Test.Integration/Step8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Step8
+    {
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+        private IDoor _door;
+        private ILight _light;
+        private ITimer _timer;
+        private IOutput _output;
+        private Display _display;
+        private PowerTube _powerTube;
+        private CookController _cookController;
+        private UserInterface _userInterface;
+
+        [SetUp]
+        public void Setup()
+        {
+            _powerButton = new Button();
+            _timeButton = new Button();
+            _startCancelButton = new Button();
+            _door = new Door();
+            
+            _output = Substitute.For<IOutput>();
+            _light = new Light(_output);
+            _timer = Substitute.For<ITimer>(); //Stubbed to be able to raise events when needed
+
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
+            
+            _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+        }
+    }
+}
